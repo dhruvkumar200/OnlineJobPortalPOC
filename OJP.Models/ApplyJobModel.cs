972640ebdfad2a8cfa8d4eff314e2358519c227f
dhruvkumar200 <dhruv.kumar@netsolutions.com>
2023-04-14 17:12:36 +0530

    14april payment gateway implemented

--- OJP.Models/ApplyJobModel.cs
+++ OJP.Models/ApplyJobModel.cs
@@ -3,10 +3,10 @@ using System.ComponentModel.DataAnnotations;
 namespace OJP.Models
 {
     public class ApplyJobModel
+
     {
         
         public int Id { get; set; }
-        
         public int ApplyBy { get; set; }
         
         public int JobPostId { get; set; }
